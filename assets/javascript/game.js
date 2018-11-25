b88cc84cab3a06c90ad623d8706e746de4793aab
Sarah Kinneer <kinneers@gmail.com>
2018-11-24 21:28:32 -0500

    js updated to randomly choose a word from a given array

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+// create variables
+//I don't know if I need this, but I'll go ahead and make it: var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var possibleWords = ['picnic', 'television', 'car', 'couch', 'chicken', 'lunchbox', 'computer', 'superman', 'dresser', 'tofu']; //Place the possible words to be picked from in this array
+var guessedLetters = []; //Empty array ready for guessed letters
+
+word = possibleWords[Math.floor(Math.random() * 10)] //Replace 10 with the number of items in the possibleWords array 
+
+// create object with everything needed
+
+console.log(word);
+
+//make array 
