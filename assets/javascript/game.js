c7d9f53a3943f4387f2139599b73a3407e2a5aee
Sarah Kinneer <kinneers@gmail.com>
2018-12-03 21:36:14 -0500

    Problem with an earlier commit- attempting a clean commit of completed project- except for media queries

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-var possibleWords = ['Christmas tree!', 'The North Pole!', 'snowball fight!', 'gingerbread house!']; //Array of possible words/phrases
+var possibleWords = ['nutcracker', 'Christmas trees', 'The North Pole', 'snowball fight', 'gingerbread house', 'winter wonderland', 'candy canes', 'stockings hung by the chimney', 'chestnuts roasting', 'St. Nicholas', 'Santa Claus is coming to town!', 'Rudolph', 'eight tiny reindeer', 'up on the housetop', "elves in Santa's workshop", 'ornaments', 'figgy pudding', 'coal in your stocking', 'presents under the tree', 'sleigh rides', 'White Christmas', 'kissing under the mistletoe', 'Silent Night', 'All I Want for Christmas is My Two Front Teeth', 'Grandma Got Run Over by a Reindeer']; //Array of possible words/phrases
 var currentWord = ""; //variable to hold the current word/phrase as a string
 var guess = ""; //string ready to hold current guessed letter
 var display = ""; //String ready to display the game board on the page
@@ -34,24 +34,30 @@ document.onkeyup = function(event) {
 
         if (letterCheck.test(guess)) { //Checks that the guess is a letter, returns boolean
             if ((guessedLetters.indexOf(guess.toUpperCase()) > -1) && (displayWord.indexOf(guess) === -1) && (displayWord.indexOf(guess.toUpperCase()) === -1)) { //Logic: guess is already in incorrectly guessed array and is not in the display word (correct letters) array
-                console.log("You have already guessed this letter."); //Future idea- play a whah-wha sound or a beat
+                console.log("You have already guessed this letter."); //This incorrect letter has been guessed
+                document.getElementById("soundLose").play(); //Plays snowball splat audio
             }
+
             else if ((guessedLetters.indexOf(guess.toUpperCase()) === -1) && (displayWord.indexOf(guess) === -1) && (displayWord.indexOf(guess.toUpperCase()) === -1)) { //Logic: the guess has not already been guessed and is not in the display word (correct letters) array 
                 guessedLetters.push(guess.toUpperCase()); //Adds the incorrect letter to the guessedLetters array as an uppercase letter
                 guessesLeft--; //Decrements the number of guesses
                 guessCountText.textContent = "Guesses Remaining: " + guessesLeft; //Updates the guesses left on the page
                 //There might be a way to add a space into the guessedLetters array (use concat or maybe join)- but I'll try this once everthing else is working
                 guessedLettersText.textContent = "Incorrect Guesses: " + guessedLetters; //Updates the guessed letters shown on the page
+                document.getElementById("soundLose").play(); //Plays snowball splat audio
             }
+
             else { //By process of elimination, this must be a correct letter
                 for (var j = 0; j < currentWord.length; j++) { //Begins a loop that will write the correct guess into the display string
                     if (currentWord.toLowerCase()[j] === guess) { //Finds the index on each loop of the correct guess
                         display = display.substr(0,j) + currentWord[j] + display.substr(j+1); //Logic: Splits the current display into two substrings, one starting from index 0 and going length j (so ending just before the correctly guessed letter); one starting at j+1, which is the index just after j; and using the currentWord[j] to update the correctly guessed letter in order to preserve original capitalization choice 
                     }
+                document.getElementById("soundWin").play(); //Plays jingle bells ringing
                 }
                 answerText.textContent = display; //Displays the game board on the page
             }
         }
+
         else {
             console.log("Not a valid input. Choose a letter."); //Logs message in console that non-letter was selected
         }
@@ -60,13 +66,14 @@ document.onkeyup = function(event) {
         if (display.indexOf("_") === -1) { //When there are no more blanks in the display the user has won
             wins++; //Increments the number of wins
             winCountText.textContent = "Wins: " + wins; //Displays number of wins
-            //Play a sound
+            document.getElementById("soundWin").play(); //Plays jingle bells ringing
+
         }
 
         //User Loses
         if (guessesLeft === 0) {
             console.log("You lost");
-            //Play a losing sound
+            document.getElementById("soundLose").play(); //Plays snowball splat audio
         }
 
         //Restart Game after either winning or losing
@@ -97,12 +104,3 @@ document.onkeyup = function(event) {
         }
     }
 }
-
-//Bonus: Play a sound or song when the user guesses their word correctly, like in our demo.
-//Bonus: Write some stylish CSS rules to make a design that fits your game's theme.
-
-//HARD MODE: Organize your game code as an object, except for the key events to get the letter guessed. This will be a challenge if you haven't coded with JavaScript before, but we encourage anyone already familiar with the language to try this out.
-    //Save your whole game and its properties in an object.
-    //Save any of your game's functions as methods, and call them underneath your object declaration using event listeners.
-    //Don't forget to place your global variables and functions above your object.
-    //Remember: global variables, then objects, then calls.
